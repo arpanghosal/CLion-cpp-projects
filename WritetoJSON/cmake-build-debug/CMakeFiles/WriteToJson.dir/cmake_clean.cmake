529988f025171e746a25ba95ed0886c88b8007ce
Arpan Ghosal <arpanghosal@Arpans-MacBook-Pro.local>
2021-01-18 22:50:52 +0100

    JSON

---
+++ WritetoJSON/cmake-build-debug/CMakeFiles/WriteToJson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WriteToJson.dir/main.cpp.o"
+  "WriteToJson"
+  "WriteToJson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WriteToJson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
