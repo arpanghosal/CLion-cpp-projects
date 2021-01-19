b51905822dfb7d26bed0121ddebcb143891177f8
Arpan Ghosal <arpanghosal@Arpans-MacBook-Pro.local>
2021-01-20 00:38:09 +0100

    json dump tryout

---
+++ dump_json/cmake-build-debug/CMakeFiles/dump_json.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dump_json.dir/main.cpp.o"
+  "dump_json"
+  "dump_json.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dump_json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- dump_json/main.cpp
+++ dump_json/main.cpp
@@ -12,19 +12,27 @@ int main() {
   std::vector<std::string> vec;
   std::string amudaage = "twenty seven";
   vec.push_back("Amu da");
-  std::string arpanage = " twenty five";
+  std::string arpanage = "twenty five";
   vec.push_back("Buddha");
 
   std::vector<std::string> vec1 = vec;
 
-  map.insert(std::pair<std::string, std::vector<std::string>>(amudaage, vec));
-  map.insert(std::pair<std::string, std::vector<std::string>>(arpanage, vec1));
-
+  map.insert(std::pair<std::string, std::vector<std::string>>(arpanage, vec));
+  map.insert(std::pair<std::string, std::vector<std::string>>(amudaage, vec1));
   std::map<std::string, std::vector<std::string>>::iterator itr;
+  for (itr=map.begin();itr!=map.end();++itr) {
+    std::cout << itr->first << std::endl;
+    for(auto c : itr->second)
+    {
+      std::cout << c << std::endl;
+    }
+    std:: cout<< std::endl;
+    //std::cout << itr->second << std::endl;
+  }
 
   for(itr = map.begin(); itr != map.end(); ++itr){
-    j["name"] = itr->first; // name = rod name
-    j["vec"] = itr->second; // vec = mod name
+    j["name"] = itr->first; // name = mod name
+    j["vec"] = itr->second; // vec = rod name
 
     o << std::setw(4) << j << std::endl;
   }
